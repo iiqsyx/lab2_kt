f901693a75ed4b1d8adb7ca24820d539b900213e
iiqsyx <alex_iiqsyx@mail.ru>
2024-02-08 00:13:24 +0300

    featback: adding kt files

--- app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.android.lab1.app
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
@@ -19,6 +20,11 @@ fun FundamentalsApp() {
                 is Screen.Buttons -> ExploreButtonsScreen()
                 is Screen.ProgressIndicator -> ProgressIndicatorScreen()
                 is Screen.AlertDialog -> AlertDialogScreen()
+                is Screen.Row -> RowScreen()
+                is Screen.Column -> ColumnScreen()
+                is Screen.Box -> BoxScreen()
+                is Screen.Surface -> SurfaceScreen()
+                is Screen.Scaffold -> ScaffoldScreen()
             }
         }
     }
